9d965fcf5cec577d07c034adfa06b3ac250fb0ad
Rodrigo_Cristales <rodrigo10cristales@gmail.com>
2025-01-02 22:47:20 -0600

    Cambios

---
+++ Login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/login.css">
+  
+    <title>Iniciar sesion</title>
+</head>
+<body>
+    <main>
+        <section>
+        <h1>Iniciar Sesion</h1>
+        <form action="" class="Estilos-form">
+            <label>Email</label>
+            <input class="Estilos-Input" type="text" placeholder="Ingrese su email">
+
+            <label>Ingrese su contraseña</label>
+            <input class="Estilos-Input" type="password" placeholder="Ingrse una contraseña">
+
+            <p class="Estilos-Parrafo">No tienes cuenta?<a href="Login.php">Crear cuenta</a></p>
+
+            <button class="Estilos-Button" type="submit">Iniciar sesion</button>
+        </form>
+        </section>
+    </main>
+</body>
+</html>
---
+++ SignUp.php
@@ -0,0 +1,32 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/login.css">
+    <title>Ospedajes sv</title>
+</head>
+<body>
+        <main>
+            <section>
+                <h1>Crear cuenta</h1>
+                <form action="" class="Estilos-form">
+                    <label>Nombre</label>
+                    <input class="Estilos-Input" type="text" placeholder="Nombre de usuario">
+
+                    <label>Email</label>
+                    <input class="Estilos-Input" type="text" placeholder="Ingrese su email">
+
+                    <label>Ingrese su contraseña</label>
+                    <input class="Estilos-Input" type="password" placeholder="Ingrse una contraseña">
+
+                    <p class="Estilos-Parrafo">Ya tienes una cuenta?<a href="Login.php">Iniciar sesion</a></p>
+
+                    <button class="Estilos-Button" type="submit">Crear cuenta</button>
+                </form>
+            </section>
+        </main>
+</body>
+</html>
